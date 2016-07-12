151008bf5c712eaa38e468cf8688505090746b82
Omar Castro <ocastro24@yahoo.com>
2016-07-12 11:41:58 -0600

    First Commit

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    logger = require('morgan'),
+    cors = require('cors'),
+    path = require('path'),
+    port = process.env.PORT || 8080
+    // apiRoutes = require('./api_routes'),
+    // mongoose = require('mongoose')
+    // databaseURL = 'mongodb://localhost:27017/testUsers'
+
+// mongoose.connect(databaseURL, function (err) {
+//   console.log('connected to THE MONGODS')
+// })
+
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(cors())
+// app.use('/api/v1', apiRoutes)
+// app.use(express.static(path.join(__dirname, './public')))
+// http://localhost:3000/api/v1/cars
+app.listen(port, function () {
+  console.log('Server running on port: ', port)
+})
